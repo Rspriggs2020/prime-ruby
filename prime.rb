6effa11e37b1af40aeede82d31db5265672a2a2a
ryliespriggs <rspriggs812@gmail.com>
2021-03-03 14:13:54 -0800

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(integer)
+    index = 2
+    if integer > 1
+        range = (index..integer-1).to_a
+        range.none? do |num_to_test|
+            integer % num_to_test == 0
+        end
+    else
+        false
+    end
+
+end
